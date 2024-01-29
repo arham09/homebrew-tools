8ce1c113bc21b38f5ba7988d964c9bc8d3bed5ef
Arham Abiyan <arham.abiyan@gmail.com>
2024-01-29 16:05:59 +0700

    fix installation in formula

--- cal-idn.rb
+++ cal-idn.rb
@@ -1,16 +1,15 @@
 class CalIdn < Formula
     desc "Indonesian Calendar CLI"
-    homepage "https://github.com/arham09/cal-idn"
-    url "https://github.com/arham09/cal-idn/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "f366b6be9f43f5e166c698e6805c47e0868d2eb1"
+    go_resource "github.com/arham09/cal-idn" do
+        url "https://github.com/arham09/cal-idn.git",
+            :revision => "03633db83450b4a695e57c7f0632ca6a1304c152"  # Replace with the specific commit or version
+      end
   
     depends_on "go" => :build
   
     def install
-      ENV["GOPATH"] = buildpath
-      (buildpath/"src/github.com/arham09/cal-idn").install Dir["*"]
-      system "go", "build", "-o", "#{bin}/cal-idn", "-v", "github.com/arham09/cal-idn"
-    end
+        system "go", "build", "-o", "#{bin}/cal-idn", "main.go"
+      end
   
     test do
       # Test your Golang program
